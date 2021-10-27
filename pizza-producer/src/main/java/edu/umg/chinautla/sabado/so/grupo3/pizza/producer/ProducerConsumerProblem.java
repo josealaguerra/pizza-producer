fbfcac86614a4fb32f7dcbd14e14de2c78b975e5
José Guerra <josealaguerra@gmail.com>
2021-10-27 04:01:16 -0600

    Update ProducerConsumerProblem.java

--- pizza-producer/src/main/java/edu/umg/chinautla/sabado/so/grupo3/pizza/producer/ProducerConsumerProblem.java
+++ pizza-producer/src/main/java/edu/umg/chinautla/sabado/so/grupo3/pizza/producer/ProducerConsumerProblem.java
@@ -9,5 +9,16 @@ package edu.umg.chinautla.sabado.so.grupo3.pizza.producer;
  * @author jag
  */
 public class ProducerConsumerProblem {
-    
+
+    public void main(String []args){
+		
+		Producer p = new Producer();
+		p.setName("Chef 1");
+		p.setStart();
+		
+		Consumer c = new Consumer(p);
+		c.setName("Cliente 1");
+		c.setStart();
+		
+	}
 }
