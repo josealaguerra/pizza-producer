84bd2f6d80d3557b1df3c126e24e9e56835042e9
Jose Guerra <josealaguerra@gmail.com>
2021-10-27 14:14:04 -0600

    Add PCS Producer Consumer Solution

---
+++ pizza-pcb/src/main/java/edu/umg/chinautla/sabado/so/grupo3/pizza/pcb/ProducerConsumerSolution.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.umg.chinautla.sabado.so.grupo3.pizza.pcb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jag
+ */
+public class ProducerConsumerSolution {
+
+    private final List<String> messages = new ArrayList<>();
+
+    public static void main(String args[]){
+
+
+    }
+
+
+
+    
+}
